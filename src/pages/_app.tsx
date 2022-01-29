bf0e57f10cc8298526881b1b79ec02678ae3a755
Rikieri Sartor <riki_eri@hotmail.com>
2022-01-29 17:29:09 -0300

    Adds theme provider and initial color pallete

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import GlobalStyle from "@/styles/globals";
 import "@/styles/globals.css";
+import { defaultTheme } from "@/styles/theme";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { DefaultTheme } from "styled-components";
+
+export const defaultTheme: DefaultTheme = {
+  palette: {
+    common: {
+      white: "#FFFFFF",
+      gray: "##E8EFF5",
+      black: "#222831",
+    },
+    primary: {
+      light: "#1C5D9F",
+      main: "#022B54",
+    },
+    secondary: {
+      light: "#D1EFF2",
+      main: "#53C3D0",
+      dark: "#34AEBC",
+    },
+  },
+};
