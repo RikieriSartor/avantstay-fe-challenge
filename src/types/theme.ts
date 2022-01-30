21375d52359891bc34c28574f5de2a31a48e5c04
Rikieri Sartor <riki_eri@hotmail.com>
2022-01-30 10:39:22 -0300

    Adds theme type

---
+++ src/types/theme.ts
@@ -0,0 +1,18 @@
+export interface Theme {
+  palette: {
+    common: {
+      white: string;
+      gray: string;
+      black: string;
+    };
+    primary: {
+      light: string;
+      main: string;
+    };
+    secondary: {
+      light: string;
+      main: string;
+      dark: string;
+    };
+  };
+}
