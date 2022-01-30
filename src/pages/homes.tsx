4951afa4c250e2b42ca8dc37a1f134001c094a9e
Rikieri Sartor <riki_eri@hotmail.com>
2022-01-30 11:26:10 -0300

    Adds basic SEO configs

--- src/pages/homes.tsx
+++ src/pages/homes.tsx
@@ -1,9 +1,16 @@
 import Page from "@/containers/page";
+import { NextSeo } from "next-seo";
 import React from "react";
 
 export default function Homes() {
   return (
     <Page>
+      <NextSeo
+        title="AvantStay: Vacation Rentals, Beach Houses, Ski Resorts, Cabins, and
+        Short Term Rentals"
+        description="Travel with the people you love. Group travel has never been this easy - check out our homes and locations across the country."
+      />
+
       <h2>Content here</h2>
     </Page>
   );
