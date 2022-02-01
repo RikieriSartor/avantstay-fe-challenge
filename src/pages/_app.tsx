b979c7825dd86f1f3c5fd3bfc71ada313b6e8ab3
Rikieri Sartor <riki_eri@hotmail.com>
2022-02-01 18:08:41 -0300

    Adds basic config of Apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
+import apolloClient from "@/services/apollo";
 import GlobalStyle from "@/styles/globals";
 import "@/styles/globals.css";
 import { defaultTheme } from "@/styles/theme";
+import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ApolloProvider>
     </ThemeProvider>
   );
 }
--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -2,9 +2,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const API_URI = process.env.NEXT_PUBLIC_API_URI;
 
-const client = new ApolloClient({
+const apolloClient = new ApolloClient({
   uri: API_URI,
   cache: new InMemoryCache(),
 });
 
-export default client;
+export default apolloClient;
