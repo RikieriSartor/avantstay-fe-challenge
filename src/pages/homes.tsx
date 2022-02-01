063850149f0fb281fe3947f80c68254906016fd2
Rikieri Sartor <riki_eri@hotmail.com>
2022-01-31 22:19:48 -0300

    Adds WIP of homes page

--- src/pages/homes.tsx
+++ src/pages/homes.tsx
@@ -1,8 +1,14 @@
+import Header from "@/components/homes/header";
+import SearchStats from "@/components/homes/search-stats";
+import Main from "@/containers/main";
 import Page from "@/containers/page";
 import { NextSeo } from "next-seo";
 import React from "react";
+import LoadingList from "@/components/homes/loading-list";
 
 export default function Homes() {
+  const isLoading = true;
+
   return (
     <Page>
       <NextSeo
@@ -11,7 +17,13 @@ export default function Homes() {
         description="Travel with the people you love. Group travel has never been this easy - check out our homes and locations across the country."
       />
 
-      <h2>Content here</h2>
+      <Header />
+
+      <Main>
+        <SearchStats count={32} isLoading={isLoading} />
+
+        {isLoading ? <LoadingList /> : <>...</>}
+      </Main>
     </Page>
   );
 }
